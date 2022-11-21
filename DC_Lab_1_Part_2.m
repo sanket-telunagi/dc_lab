a4892580c2b4815e54e470908a0e1aa5c0326a6c
sanket-telunagi <110254677+sanket-telunagi@users.noreply.github.com>
2022-11-22 00:23:49 +0530

    The final codes

---
+++ DC_Lab_1_Part_2.m
@@ -0,0 +1,13 @@
+y=normrnd(0,1,1,200);
+Gy=periodogram(y);
+Ry=abs(ifft(Gy,256));
+Ry=[Ry(130:256)' Ry(1:129)']
+t=-127:1:128;
+subplot(1,2,1)
+plot(Gy)
+xlabel('Frequency Samples');
+title('PSD')
+subplot(1,2,2)
+plot(t,Ry)
+xlabel('Time Shift')
+title('Autocorrelation')
